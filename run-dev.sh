d39fcd9030e07d4d35f2b096c96bf47483712d6a
wengzt <wengzt@ofidc.com>
2023-06-24 02:13:36 +0800

    other

--- run-dev.sh
+++ run-dev.sh
@@ -5,6 +5,7 @@ mkdir -p /tmp/admin-log
 export log_dir=/tmp/admin-log
 export now_time=`date +%Y-%m-%d-%H-%M-%S`
 export C_FORCE_ROOT=true
+export PYTHONUNBUFFERED=1
 export APP_CONFIG=Env
 
 python generate_config.py
--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ mkdir -p ./logs
 export log_dir=./logs
 export now_time=`date +%Y-%m-%d-%H-%M-%S`
 export C_FORCE_ROOT=true
+export PYTHONUNBUFFERED=1
 export APP_CONFIG=Env
 
 cd /app/
